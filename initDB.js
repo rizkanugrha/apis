2784ab438e451ea1d0bb8b7694ca95a1583087b6
Rizka Nugraha <68932997+rizkanugrha@users.noreply.github.com>
2025-01-12 06:11:39 +0700

    Update initDB.js

--- initDB.js
+++ initDB.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
 module.exports = () => {
+  mongoose.set("strictQuery", true); // Pengaturan strict mode untuk query
   mongoose
     .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
     })
     .then(() => {
-      console.log('Mongodb connected....');
+      console.log("MongoDB connected...");
     })
-    .catch(err => console.log(err.message));
+    .catch((err) => console.error("MongoDB connection error:", err.message));
 
-  mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to db...');
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to DB.");
   });
 
-  mongoose.connection.on('error', err => {
-    console.log(err.message);
+  mongoose.connection.on("error", (err) => {
+    console.error("Mongoose error:", err.message);
   });
 
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose connection is disconnected...');
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose disconnected from DB.");
   });
 
-  process.on('SIGINT', () => {
+  process.on("SIGINT", () => {
     mongoose.connection.close(() => {
-      console.log(
-        'Mongoose connection is disconnected due to app termination...'
-      );
+      console.log("Mongoose connection closed due to app termination.");
       process.exit(0);
     });
   });
