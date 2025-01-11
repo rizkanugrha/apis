23f72b827a5848ffd494139139017755a2f1c765
Rizka Nugraha <68932997+rizkanugrha@users.noreply.github.com>
2025-01-12 06:43:42 +0700

    Update banjirControllers.js

--- Controllers/banjirControllers.js
+++ Controllers/banjirControllers.js
@@ -1,44 +1,93 @@
-const { BanjirModels } = require("../Models/banjirModels");
-const createError = require("http-errors");
-const mongoose = require("mongoose");
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+const { BanjirModels } = require('../Models/banjirModels');
 
 const getAllBanjir = async (req, res, next) => {
   try {
-    const results = await BanjirModels.find();
-    res.json(results);
+    const results = await BanjirModels.find({}, { __v: 0 });
+    res.send(results);
   } catch (error) {
+    console.error(error.message);
     next(error);
   }
 };
 
 const createNewBanjir = async (req, res, next) => {
   try {
-    const newBanjir = new BanjirModels(req.body);
-    const savedBanjir = await newBanjir.save();
-    res.json(savedBanjir);
+    const banjir = new BanjirModels(req.body);
+    const result = await banjir.save();
+    res.send(result);
   } catch (error) {
+    console.error(error.message);
+    if (error.name === 'ValidationError') {
+      next(createError(422, error.message));
+      return;
+    }
+    next(error);
+  }
+};
+
+const findBanjirById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw createError(400, 'Invalid Banjir ID');
+    }
+
+    const banjir = await BanjirModels.findById(id);
+    if (!banjir) {
+      throw createError(404, 'Banjir record does not exist.');
+    }
+    res.send(banjir);
+  } catch (error) {
+    console.error(error.message);
     next(error);
   }
 };
 
 const updateBanjir = async (req, res, next) => {
   try {
-    const updated = await BanjirModels.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updated) throw createError(404, "Data tidak ditemukan.");
-    res.json(updated);
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw createError(400, 'Invalid Banjir ID');
+    }
+
+    const updatedBanjir = await BanjirModels.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedBanjir) {
+      throw createError(404, 'Banjir record does not exist.');
+    }
+    res.status(200).json(updatedBanjir);
   } catch (error) {
+    console.error(error.message);
     next(error);
   }
 };
 
 const deleteBanjir = async (req, res, next) => {
+  const id = req.params.id;
   try {
-    const deleted = await BanjirModels.findByIdAndDelete(req.params.id);
-    if (!deleted) throw createError(404, "Data tidak ditemukan.");
-    res.json({ message: "Data berhasil dihapus.", _id: deleted._id });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw createError(400, 'Invalid Banjir ID');
+    }
+
+    const result = await BanjirModels.findByIdAndDelete(id);
+    if (!result) {
+      throw createError(404, 'Banjir record does not exist.');
+    }
+    res.send(result);
   } catch (error) {
+    console.error(error.message);
     next(error);
   }
 };
 
-module.exports = { getAllBanjir, createNewBanjir, updateBanjir, deleteBanjir };
+module.exports = {
+  getAllBanjir,
+  createNewBanjir,
+  findBanjirById,
+  updateBanjir,
+  deleteBanjir,
+};
