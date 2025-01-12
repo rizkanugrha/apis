5855d2ac85b272dfbda1649a070106dc7829dd26
Yudha ps <68932997+rizkanugrha@users.noreply.github.com>
2025-01-12 07:53:34 +0700

    Update banjirModels.js

--- Models/banjirModels.js
+++ Models/banjirModels.js
@@ -1,36 +1,12 @@
 const mongoose = require("mongoose");
 
-const banjirSchema = new mongoose.Schema(
-  {
-    bencana: {
-      type: String,
-      default: "Banjir",
-    },
-    waktu: {
-      type: Date,
-      required: [true, "Waktu kejadian wajib diisi"],
-    },
-    lokasi: {
-      type: String,
-      required: [true, "Lokasi kejadian wajib diisi"],
-    },
-    penyebab: {
-      type: String,
-      required: [true, "Penyebab wajib diisi"],
-    },
-    kronologi: {
-      type: String,
-      required: [true, "Kronologi wajib diisi"],
-    },
-    pengungsi: {
-      type: Number,
-      default: 0,
-      min: [0, "Jumlah pengungsi tidak boleh negatif"],
-    },
-  },
-  { timestamps: true }
-);
+const banjirSchema = new mongoose.Schema({
+  bencana: { type: String, required: [true, "Bencana wajib diisi"] },
+  waktu: { type: Date, required: [true, "Waktu kejadian wajib diisi"] },
+  lokasi: { type: String, required: [true, "Lokasi wajib diisi"] },
+  penyebab: String,
+  kronologi: String,
+  pengungsi: { type: Number, default: 0, min: [0, "Jumlah pengungsi tidak boleh negatif"] },
+});
 
-const BanjirModels = mongoose.model("Banjir", banjirSchema);
-
-module.exports = { BanjirModels };
+module.exports = mongoose.model("Banjir", banjirSchema);
